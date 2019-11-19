582e6ff2ace4f6f29d9f517c0d6963433074e170
AnthonyTB <Anthonylol@protonmail.com>
2019-11-19 09:44:22 -0800

    progress on rendering folders

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,12 +3,22 @@ import "../styles/Sidebar.css";
 import STORE from "./Store";
 
 export default class Sidebar extends React.Component {
+  renderfolder() {
+    const folderId = STORE.folders.filter(id => {
+      if (id === STORE.notes.folderId) {
+        return folderId;
+      }
+    });
+  }
+
   render() {
     const folders = STORE.folders.map(folder => {
       return (
-        <li className="foldersList">
-          <h2>{folder.name}</h2>
-        </li>
+        <button className="folderBtn" onClick={this.renderFolder}>
+          <li className="foldersList">
+            <h2>{folder.name}</h2>
+          </li>
+        </button>
       );
     });
     return (
